64d0c741694b700a098b6a466a67fdf139bfc3bb
Arin Dev <arinpandey01@gmail.com>
2026-09-06 08:37:51 +0530

    feat: add entry points for juni

--- cmd/juni/main.go
+++ cmd/juni/main.go
@@ -1,9 +1,51 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"os"
 )
 
 func main() {
+	if len(os.Args) > 2 {
+		fmt.Println("Usage: juni [script]")
+		os.Exit(64)
+	}
 
+	if len(os.Args) == 2 {
+		runFile(os.Args[1])
+		return
+	}
+
+	runPrompt()
+}
+
+func runFile(path string) {
+	source, err := os.ReadFile(path)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(65)
+	}
+
+	run(source)
+}
+
+func runPrompt() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("> ")
+		if !scanner.Scan() {
+			break
+		}
+
+		run(scanner.Bytes())
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
+}
+
+func run(source []byte) {
+	// Interpreter logic will be here
 }
